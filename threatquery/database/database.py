6032769cddfec98cf5be9d2ec05461144331b8f6
Emre Eren <16957951+emreren@users.noreply.github.com>
2025-03-06 14:59:52 +0300

    fix: Database connection issue in Docker containers

--- threatquery/database/database.py
+++ threatquery/database/database.py
@@ -1,11 +1,14 @@
 # threatquery/database/database.py file
 
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from threatquery.database.models import Base
 from config.env_config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
+# Use environment variable or fallback to the config value
+db_url = os.getenv("DATABASE_URL", DATABASE_URL)
+engine = create_engine(db_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base.metadata.create_all(bind=engine)
